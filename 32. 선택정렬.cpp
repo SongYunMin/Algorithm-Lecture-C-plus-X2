ca62fd75d9362752e65258c6e2338b54e36afdd4
YunMin-Song <thd930308@naver.com>
2020-09-09 18:27:23 +0900

    Algorithm Update

---
+++ 32. 선택정렬.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, i, j, temp;
+	int a[100];
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+	}
+
+
+	return 0;
+}
---
+++ 56. 재귀함수 분석.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void recur(int x)
+{
+	if (x == 0) return;
+	else {
+		// 여기다 printf 하면 3 2 1 이 출력 됨
+		recur(x - 1);
+		printf("%d ", x);
+	}
+}
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+
+	recur(n);
+
+	return 0;
+}
