a6de514eba24f5b1b0e2a065c3d509189e30fd5a
YunMin-Song <thd930308@naver.com>
2020-09-09 17:34:06 +0900

    Algorithm Update

--- 17. 선생님 퀴즈.cpp
+++ 17. 선생님 퀴즈.cpp
@@ -7,11 +7,10 @@ int main(void)
 	for (i = 1; i <= n; i++) {
 		scanf("%d %d", &m, &ans);
 		sum = 0;
-		for (j = 1; j <= m; j++) {
-			sum += j;
-
+		for (j = 1; j <= m; j++) {		
+			sum += j;					// j부터 m의 합까지 더함
 		}
-		if (ans == sum) printf("YES\n");
+		if (ans == sum) printf("YES\n");// 제출한 답이 맞는지 확인 함
 		else printf("NO\n");
 	}
 
