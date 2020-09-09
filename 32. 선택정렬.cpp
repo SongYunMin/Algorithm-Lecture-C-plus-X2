eab9ae3aa2fb464704a7efaf9c31266b74b8e27c
YunMin-Song <thd930308@naver.com>
2020-09-09 22:55:44 +0900

    Update

--- 32. 선택정렬.cpp
+++ 32. 선택정렬.cpp
@@ -2,10 +2,26 @@
 
 int main(void)
 {
-	int n, i, j, temp;
+	int n, i, j, temp, index;
 	int a[100];
 	scanf("%d", &n);
 	for (i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (i = 0; i < n-1; i++) {
+		index = i;
+		for (j = i + 1; j < n; j++) {
+			if (a[j] < a[index]) index = j;
+		}
+		temp = a[i];
+		a[i] = a[index];
+		a[index] = temp;
+	}
+
+
+	for (i = 0; i < n; i++) {
+		printf("%d ", a[i]);
 	}
 
 
