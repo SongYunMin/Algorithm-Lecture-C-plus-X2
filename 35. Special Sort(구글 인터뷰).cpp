9707984090b6376a77d9eeff0cee1f79878f0f14
YunMin-Song <thd930308@naver.com>
2020-09-10 18:41:56 +0900

    Update

---
+++ 35. Special Sort(구글 인터뷰).cpp
@@ -0,0 +1,30 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int n, i, j, index, temp;
+//	int a[101];
+//
+//	scanf("%d", &n);
+//	for (i = 0; i < n; i++) {
+//		scanf("%d", &a[i]);
+//	}
+//
+//	for (i = 0; i < n; i++) {
+//		for (j = 0; j < n - 1; j++) {
+//			// 앞이 양수고 뒤에가 음수면 Swap
+//			if (a[j] > 0 && a[j + 1] < 0) {
+//				temp = a[j];
+//				a[j] = a[j + 1];
+//				a[j + 1] = temp;
+//			}
+//
+//		}
+//	}
+//
+//	for (i = 0; i < n; i++) {
+//		printf("%d ", a[i]);
+//	}
+//
+//	return 0;
+//}
--- 59. 부분집합(DFS).cpp
+++ 59. 부분집합(DFS).cpp
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
+int n, ch[11];
+void DFS(int L)
+{
+	int i;
+	if (L == n + 1) {
+		for (i = 1; i <= n; i++) {
+			if (ch[i] == 1) printf("%d", i);
+		}
+		puts("");
+	}
+	else {
+		ch[L] = 1;
+		DFS(L + 1);
+		ch[L] = 0;
+		DFS(L + 1);
+	}
+}
+
 int main(void)
 {
-	int n, i, j, index, temp;
-	int a[101];
-
 	scanf("%d", &n);
-	for (i = 0; i < n; i++) {
-		scanf("%d", &a[i]);
-	}
+	DFS(1);
 
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n-1; j++) {
-			// 앞이 양수고 뒤에가 음수면 Swap
-			if (a[j] > 0 && a[j + 1] < 0) {
-				temp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = temp;
-			}
-
-		}
-	}
-
-	for (i = 0; i < n; i++) {
-		printf("%d ", a[i]);
-	}
 
 	return 0;
 }
