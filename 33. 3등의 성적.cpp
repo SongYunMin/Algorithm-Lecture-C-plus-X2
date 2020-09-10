42597be6a242b28fae5169d502484d16b99c7c7f
YunMin-Song <thd930308@naver.com>
2020-09-10 17:49:26 +0900

    Algorithm Update

--- 33. 3등의 성적.cpp
+++ 33. 3등의 성적.cpp
@@ -1,37 +1,35 @@
-#include <stdio.h>
-
-int main(void)
-{
-	int n, i, j, index, temp, cnt = 0;
-	int a[101];
-
-	scanf("%d", &n);			// n
-	
-	for (i = 0; i < n; i++) {
-		scanf("%d",&a[i]);	// 학생의 성적
-	}
-
-	// Selection Sort
-	for (i = 0; i < n; i++) {
-		index = i;
-		for (j = i + 1; j < n; j++) {
-			if (a[index] < a[j]) index = j;
-		}
-		temp = a[i];
-		a[i] = a[index];
-		a[index] = temp;
-	}
-
-	// 3등 구하기
-	for (i = 1; i < n; i++) {
-		if (a[i - 1] != a[i]) {
-			cnt++;
-			if (cnt == 2) {
-				printf("%d", a[i]);
-				break;
-			}
-		}
-	}
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, j, cnt = 0, temp, index, n;
+//	int a[101];
+//
+//	scanf("%d", &n);				// n 입력
+//	// 요소 입력
+//	for (i = 0; i < n; i++) {
+//		scanf("%d", &a[i]);
+//	}
+//
+//	// 요소 정렬 (Selection Sort)
+//	for (i = 0; i < n; i++) {
+//		index = i;
+//		for (j = i + 1; j < n; j++) {
+//			// 내림차순
+//			if (a[index] < a[j]) index = j;
+//		}
+//		temp = a[i];
+//		a[i] = a[index];
+//		a[index] = temp;
+//	}
+//
+//	for (i = 1; i < n; i++) {
+//		if (a[i - 1] != a[i]) cnt++;
+//		if (cnt == 2) {
+//			printf("%d", a[i]);
+//			break;
+//		}
+//	}
+//	
+//	return 0;
+//}
---
+++ 57. 재기함수 이진수 출력.cpp
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//
+//
+//void D(int x) 
+//{
+//	if (x == 0) return;
+//	else {
+//		D(x / 2);
+//		printf("%d", x % 2);	// 호출 밑에 하면 제대로 출력 됨
+//	}
+//}
+//
+//int main(void)
+//{
+//	int n;
+//	scanf("%d", &n);
+//	D(n);
+//	
+//	return 0;
+//}
