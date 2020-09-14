f383ed281accadad12b4ffec13e7f48b02cf2c38
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-09-14 17:13:07 +0900

    Update

--- LINE.cpp
+++ LINE.cpp
@@ -1,57 +1,53 @@
-//#include <stdio.h>
-//#include <vector>
-//#include <iostream>
-//
-//int solution(std::vector<std::vector<int>> boxes)
-//{
-//	int answer = 0, i, j;
-//	std::vector<int> buf((int)boxes.size());
-//
-//	for (i = 0; i < (int)boxes.size(); i++) {
-//		for (j = 0; j < (int)boxes[i].size(); j++) {
-//			if(boxes[i][j] <= boxes.size())
-//			buf[boxes[i][j] - 1]++;
-//		}
-//	}
-//
-//	for (i = 0; i < buf.size(); i++) {
-//		if (buf[i] != 2) {
-//			answer++;
-//		}
-//	}
-//
-//	for (i = 0; i < buf.size(); i++) {
-//		std::cout << buf[i];
-//	}
-//
-//	return answer;
-//}
-//
-//
-//int main(void)
-//{
-//	std::vector<std::vector<int>> boxes =
-//	{
-//		{1,2},
-//		{2,1},
-//		{3,3},
-//		{4,5},
-//		{5,6},
-//		{7,8}
-//	};
-//
-//
-//
-//	//for (int i = 0; i < (int)boxes.size(); i++) {
-//	//	for (int j = 0; j < (int)boxes[i].size(); j++) {
-//	//		std::cout << boxes[i][j] << " ";
-//	//	}
-//	//	std::cout << std::endl;
-//	//}
-//
-//
-//	solution(boxes);
-//
-//
-//	return 0;
-//}
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+int solution(std::vector<std::vector<int>> boxes)
+{
+	int answer = 0, i, j;
+	std::vector<int> buf((int)boxes.size());
+
+	for (i = 0; i < (int)boxes.size(); i++) {
+		for (j = 0; j < (int)boxes[i].size(); j++) {
+			if(boxes[i][j] <= boxes.size())
+			buf[boxes[i][j] - 1]++;
+		}
+	}
+
+	for (i = 0; i < buf.size(); i++) {
+		if (buf[i] != 2) {
+			answer++;
+		}
+	}
+
+	for (i = 0; i < buf.size(); i++) {
+		std::cout << buf[i];
+	}
+
+	return answer;
+}
+
+int main(void)
+{
+	std::vector<std::vector<int>> boxes =
+	{
+		{1,2},
+		{2,1},
+		{3,3},
+		{4,5},
+		{5,6},
+		{7,8}
+	};
+
+	//for (int i = 0; i < (int)boxes.size(); i++) {
+	//	for (int j = 0; j < (int)boxes[i].size(); j++) {
+	//		std::cout << boxes[i][j] << " ";
+	//	}
+	//	std::cout << std::endl;
+	//}
+
+	solution(boxes);
+
+
+	return 0;
+}
