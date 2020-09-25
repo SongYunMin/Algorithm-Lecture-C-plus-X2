7448eae62696a54e646063d7f5c28c32f74c7dfe
YunMin-Song <thd930308@naver.com>
2020-09-25 23:17:31 +0900

    Algorithm Update

--- 29. 3의 개수는(구글 인터뷰).cpp
+++ 29. 3의 개수는(구글 인터뷰).cpp
@@ -2,6 +2,19 @@
 
 int main(void)
 {
+	int n, tmp, i, cnt = 0, digit;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		tmp = i;
+		while (tmp > 0) {
+			digit = tmp % 10;			// tmp 를 1자리 수로 분리함
+			if (digit == 3) cnt++;		// 3이면 cnt 증가
+			tmp = tmp / 10;				// tmp를 나누어서 수 줄임
+		}
+	}
+	
+	printf("%d", cnt);
 
 	return 0;
 }
