069799a9734951120191d1e0f5232198bbff1141
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-11-24 23:49:28 +0900

    Algorithm Update

--- 38. Inversion_Sequence.cpp
+++ 38. Inversion_Sequence.cpp
@@ -5,20 +5,37 @@
 
 int main(void)
 {
-	int N, i, j, pos = 0, count = 0, a = 1;
-	std::cin >> N;
-	std::vector<int> InversionSequence(N);
-	std::vector<int> OriginalSequence(N);
+	int n, i, j, pos = 0;
+	std::cin >> n;
+	std::vector<int> InversionSequence(n + 1);
+	std::vector<int> OriginalSequence(n + 1);
 
-	// 기존 수열 생성
-	for (i = 0; i < N; i++) {
+	// Inversion Sequence 입력
+	for (i = 1; i < n; i++) {
 		std::cin >> InversionSequence[i];
 	}
 
+	// IS Vector의 뒤에서 처음까지 반복함
+	for (i = n; i >= 1; i--) {
+		pos = i;	// 시작 위치
+		// j가 IS[i]가 가지고 있는 값까지 반복 함
+		for (j = 1; j <= InversionSequence[i]; j++) {
+			OriginalSequence[pos] = OriginalSequence[pos + 1];
+			pos++;
+		}
+		OriginalSequence[pos] = i;
+	}
 
-	for (i = 0; i < N; i++) {
+	// i부터 찍어줘야 함
+	for (i = 1; i <= n; i++) {
 		std::cout << OriginalSequence[i] << " ";
 	}
 
+
 	return 0;
 }
+
+/*
+	이 알고리즘의 핵심은 IS의 값을 이용하여 반복하여 IS[i]의 값만큼
+	앞으로 떙기고 IS[i]의 값만큼 반복이 끝나면 그 index에 i값을 넣어주면 됨!
+*/
--- 56. 재귀함수 분석 보충(스택 프레임).cpp
+++
@@ -1,2 +0,0 @@
-#include <iostream>
-#include "Inversion_Sequence.cpp"
