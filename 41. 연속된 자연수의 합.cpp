1817da82c1d822153f436361034f9a299ef2b13f
YunMin-Song <thd930308@naver.com>
2020-11-27 21:37:01 +0900

    Algorithm Update

--- 41. 연속된 자연수의 합.cpp
+++ 41. 연속된 자연수의 합.cpp
@@ -1,19 +1,32 @@
-//#include <iostream>
-//#include <algorithm>
-//#include <vector>
-//
-//int main(void)
-//{
-//	int e, d;
-//	e = 11;
-//	d = 0;
-//	while (1){
-//		d++;
-//		if ((e * d) % 55 == 1) {
-//			printf("%d * %d mod 40에서 1이나오는 d : %d", e, d, d);
-//			break;
-//		}
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+
+int main(void)
+{
+	int n, b = 1, cnt = 0, tmp, i;
+	std::cin >> n;
+	tmp = n;
+	n--;
+	while (n > 0) {
+		b++;
+		n -= b;
+		if (n % b == 0) {
+			for (i = 1; i < b; i++) {
+				std::cout << (n / b) + i << " + ";
+			}
+			std::cout << (n / b) + i << " = " << tmp << std::endl;
+			cnt++;
+		}
+	}
+	std::cout << cnt << std::endl;
+
+	return 0;
+}
+
+/*
+이 알고리즘은 기초 수리 문제로, 푸는 방법은 만약 15라는 수가 n이라고 가정하고, 두개로 분리하고 싶다면
+n을 1과 2를 빼본다,(12가 됨) 그 수를 2로 나눈 나머지를 구해보고 0이라면 2로 나누고 각각 1과 2에 더한다
+(1+6) = 7, (2+6) = 8
+7 + 8 = 15
+3개, 4개도 마찬가지로 나머지 연산을 이용하면 된다.
+*/
