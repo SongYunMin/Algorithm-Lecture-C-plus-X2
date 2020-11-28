5c4a77232d271f29d9f884fc73696c5e0146434d
YunMin-Song <thd930308@naver.com>
2020-11-28 12:55:32 +0900

    Algorithm Update

--- 43. 뮤직비디오(이분검색 응용).cpp
+++ 43. 뮤직비디오(이분검색 응용).cpp
@@ -1,55 +1,55 @@
-#include <iostream>
-#include <algorithm> 
-int a[1001], n;
-
-int Count(int size)
-{
-	int i, cnt = 1, sum = 0;
-	for (i = 1; i <= n; i++) {
-		// DVD 한개 용량
-		if (sum + a[i] > size) {
-			cnt++;
-			sum = a[i];
-		}
-		else {
-			sum = sum + a[i];
-		}
-	}
-	return cnt;
-}
-
-int main(void)
-{
-	int m, i, lt = 1, rt = 0, mid, res=0;
-
-	std::cin >> n >> m;
-
-	for (i = 1; i <= n; i++) {
-		std::cin >> a[i];
-		rt += a[i];			// 뮤직비디오의 총 시간을 저장
-	}
-
-	// 이분 검색
-	while (lt <= rt) {
-		mid = (lt + rt) / 2;
-		// 답으로 가능할 경우
-		if (Count(mid) <= m) {
-			res = mid;
-			rt = mid - 1;
-
-		}
-		// 답이 가능하지 않을 경우
-		else {
-			lt = mid + 1;
-		}
-	}
-
-	std::cout << res << std::endl;
-
-	return 0;
-}
-
-/*
-이분검색을 활용하여 최적의 답을 도출해낸다.
-
-*/
+//#include <iostream>
+//#include <algorithm> 
+//int a[1001], n;
+//
+//int Count(int size)
+//{
+//	int i, cnt = 1, sum = 0;
+//	for (i = 1; i <= n; i++) {
+//		// DVD 한개 용량
+//		if (sum + a[i] > size) {
+//			cnt++;
+//			sum = a[i];
+//		}
+//		else {
+//			sum = sum + a[i];
+//		}
+//	}
+//	return cnt;
+//}
+//
+//int main(void)
+//{
+//	int m, i, lt = 1, rt = 0, mid, res=0;
+//
+//	std::cin >> n >> m;
+//
+//	for (i = 1; i <= n; i++) {
+//		std::cin >> a[i];
+//		rt += a[i];			// 뮤직비디오의 총 시간을 저장
+//	}
+//
+//	// 이분 검색
+//	while (lt <= rt) {
+//		mid = (lt + rt) / 2;
+//		// 답으로 가능할 경우
+//		if (Count(mid) <= m) {
+//			res = mid;
+//			rt = mid - 1;
+//
+//		}
+//		// 답이 가능하지 않을 경우
+//		else {
+//			lt = mid + 1;
+//		}
+//	}
+//
+//	std::cout << res << std::endl;
+//
+//	return 0;
+//}
+//
+///*
+//이분검색을 활용하여 최적의 답을 도출해낸다.
+//
+//*/
--- 문제.cpp
+++ 문제.cpp
@@ -1,95 +1,52 @@
-//#include <iostream>
-//#include <string>
-//#include <vector>
-//
-///*
-//1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-//2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
-//3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
-//*/
-////
-//// Return : 정답을 가장 많이 맞힌 사람
-//// Answers : 정답
-//std::vector<int> solution(std::vector<int> answers)
-//{
-//	std::vector<int> A_array{ 1,2,3,4,5 };
-//	std::vector<int> B_array{ 2,1,2,3,2,4,2,5 };
-//	std::vector<int> C_array{ 3,3,1,1,2,2,4,4,5,5 };
-//	int A_len = 0, B_len = 0, C_len = 0;
-//
-//	std::vector<int> result;
-//	int A_count = 0, B_count = 0, C_count = 0;
-//
-//	// 정답벡터 길이 / A배열 길이
-//	if (answers.size() % A_array.size() != 0) {
-//		A_len = answers.size() / A_array.size() + 1;
-//	}
-//	else {
-//		A_len = answers.size() / A_array.size();		// 딱 나누어 떨어짐
-//	}
-//	// 정답벡터 길이 / B배열 길이
-//	if (answers.size() % B_array.size() != 0) {
-//		B_len = answers.size() / B_array.size() + 1;
-//	}
-//	else {
-//		B_len = answers.size() / B_array.size();
-//	}
-//	// 정답벡터 길이 / C배열 길이
-//	if (answers.size() % C_array.size() != 0) {
-//		C_len = answers.size() / C_array.size() + 1;
-//	}
-//	else {
-//		C_len = answers.size() / C_array.size();
-//	}
-//
-//	// 배열 이어 붙임
-//	for (int i = 1; i < A_len; i++) {
-//		A_array.insert(A_array.end(), A_array.begin(), A_array.end());
-//	}
-//	for (int i = 1; i < B_len; i++) {
-//		B_array.insert(B_array.end(), B_array.begin(), B_array.end());
-//	}
-//	for (int i = 1; i < C_len; i++) {
-//		C_array.insert(C_array.end(), C_array.begin(), C_array.end());
-//	}
-//
-//	for (int i = 0; i < answers.size(); i++) {
-//		if (answers[i] == A_array[i]) {
-//			A_count++;
-//		}
-//	}
-//
-//	for (int i = 0; i < answers.size(); i++) {
-//		if (answers[i] == B_array[i]) {
-//			B_count++;
-//		}
-//	}
-//
-//	for (int i = 0; i < answers.size(); i++) {
-//		if (answers[i] == C_array[i]) {
-//			C_count++;
-//		}
-//	}
-//
-//
-//	
-//
-//
-//
-//	return result;
-//}
-//
-//
-//int main(void)
-//{
-//	std::vector<int> test;
-//	test.push_back(1);
-//	test.push_back(2);
-//	test.push_back(3);
-//	test.push_back(4);
-//	test.push_back(5);
-//	solution(test);
-//
-//
-//	return 0;
-//}
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//핵심 소스코드의 설명을 주석으로 작성하면 평가에 큰 도움이 됩니다.
+class Solution {
+public:
+    int solution(vector<int> votes) {
+        int count = 0, max, i, index, temp;
+
+        // 후보 0이 유일한 후보라면?
+        if (votes.size() == 1) {
+            return count;
+        }
+
+        temp = votes[0];
+        votes.erase(votes.begin());
+
+        while (true) {
+            max = *max_element(votes.begin(), votes.end());
+            vector<int>::iterator itr = find(votes.begin(), votes.end(), max);
+            index = distance(votes.begin(), itr);
+
+            if (temp > max) {
+                return count;
+            }
+            else {
+                votes[index]--;
+                temp++;
+                count++;
+            }
+            
+        }
+      
+        return count;
+    }
+};
+
+int main(void)
+{
+    Solution s;
+    vector<int> test;
+    test.push_back(5);
+    test.push_back();
+    test.push_back(7);
+    test.push_back(3);
+    test.push_back(8);
+    cout<<s.solution(test);
+
+    return 0;
+}
