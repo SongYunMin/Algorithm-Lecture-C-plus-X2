13be96a181a1a38419a80ef07dd56319af131dd5
YunMin-Song <thd930308@naver.com>
2020-12-07 00:08:30 +0900

    Update Commit

--- 문제.cpp
+++ 문제.cpp
@@ -1,52 +1,52 @@
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-//핵심 소스코드의 설명을 주석으로 작성하면 평가에 큰 도움이 됩니다.
-class Solution {
-public:
-    int solution(vector<int> votes) {
-        int count = 0, max, i, index, temp;
-
-        // 후보 0이 유일한 후보라면?
-        if (votes.size() == 1) {
-            return count;
-        }
-
-        temp = votes[0];
-        votes.erase(votes.begin());
-
-        while (true) {
-            max = *max_element(votes.begin(), votes.end());
-            vector<int>::iterator itr = find(votes.begin(), votes.end(), max);
-            index = distance(votes.begin(), itr);
-
-            if (temp > max) {
-                return count;
-            }
-            else {
-                votes[index]--;
-                temp++;
-                count++;
-            }
-            
-        }
-      
-        return count;
-    }
-};
-
-int main(void)
-{
-    Solution s;
-    vector<int> test;
-    test.push_back(5);
-    test.push_back();
-    test.push_back(7);
-    test.push_back(3);
-    test.push_back(8);
-    cout<<s.solution(test);
-
-    return 0;
-}
+//#include <vector>
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+////핵심 소스코드의 설명을 주석으로 작성하면 평가에 큰 도움이 됩니다.
+//class Solution {
+//public:
+//    int solution(vector<int> votes) {
+//        int count = 0, max, i, index, temp;
+//
+//        // 후보 0이 유일한 후보라면?
+//        if (votes.size() == 1) {
+//            return count;
+//        }
+//
+//        temp = votes[0];
+//        votes.erase(votes.begin());
+//
+//        while (true) {
+//            max = *max_element(votes.begin(), votes.end());
+//            vector<int>::iterator itr = find(votes.begin(), votes.end(), max);
+//            index = distance(votes.begin(), itr);
+//
+//            if (temp > max) {
+//                return count;
+//            }
+//            else {
+//                votes[index]--;
+//                temp++;
+//                count++;
+//            }
+//            
+//        }
+//      
+//        return count;
+//    }
+//};
+//
+//int main(void)
+//{
+//    Solution s;
+//    vector<int> test;
+//    test.push_back(5);
+//    test.push_back();
+//    test.push_back(7);
+//    test.push_back(3);
+//    test.push_back(8);
+//    cout<<s.solution(test);
+//
+//    return 0;
+//}
--- 재배치.cpp
+++ 재배치.cpp
@@ -1,69 +1,35 @@
-//#include <stdio.h>
 //#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//#include <string>
-//#include <time.h>
 //
-//int n, m;	// 재배치될 20학번 = n, 멘토 인원 = m
-//// 재배치
-//std::vector<std::string> Relocation(std::vector<std::string> studentCouncil, std::vector<int> num,
-//	std::vector<std::string> freahMan)
+//void insertionSort(int* list, int n)
 //{
-//	int random_Num;
-//	std::cout << "재배치 될 학생을 배분합니다" << std::endl;
-//	srand((unsigned int)time(NULL));
-//	for (int i = 0; i < n; i++) {
-//		random_Num = rand() % m + 0;
-//		if (num[random_Num] < 4) {
-//			num[random_Num]++;
+//	int i, j, key;
+//	for (i = 1; i < n; i++) {
+//		key = list[i];
+//		for (j = i - 1; j >= 0 && list[j] > key; j--) {
+//			list[j + 1] = list[j];
 //		}
-//		for (int j = 0; j < n; j++) {
-//			//if (random_Num[i] == random_Num[j]) {
-//			//	i--;
-//			//	break;
-//			//}
-//		}
+//		list[j + 1] = key;
 //	}
+//
 //}
 //
 //int main(void)
-//{			
-//	std::cout << "재배치될 학생수와 기존 멘토인원을 입력하세요" << std::endl;
-//	std::cin >> n >> m;
+//{
+//	int list[6] = { 3,5,8,1,2,7 };
 //
-//	// Dynamic Allocation (Vector Container)
-//	std::vector<std::string> freshMan(n);				// 재배치 인원
-//	std::vector<std::string> studentCouncil(m);			// 학생회 인원
-//	std::vector<int> studentCouncil_Num(m);
-//
-//	std::cout << "멘토 이름과 맡고있는 멘티 수를 입력하세요. (String, Integer)" << std::endl;
-//	for (int i = 0; i < m; i++) {
-//		std::cin >> studentCouncil[i] >> studentCouncil_Num[i];
+//	std::cout << "정렬 되기 전 : " << std::endl;
+//	for (int i = 0; i < 6; i++) {
+//		std::cout << list[i];
 //	}
-//
-//	std::cout << "멘토와 맡고있는 인원수는 아래와 같습니다." << std::endl;
-//	for (int i = 0; i < m; i++) {
-//		std::cout << "이름 : " << studentCouncil[i] << "  멘티 수 : " << studentCouncil_Num[i] << std::endl;
-//	}
-//
 //	std::cout << std::endl;
 //
-//	std::cout << "멘티 이름을 입력하여 주세요." << std::endl;
-//	for (int i = 0; i < n; i++) {
-//		std::cout << i << "번째 (총" << n << "명) ";
-//		std::cin >> freshMan[i];
+//	insertionSort(list, 6);
+//
+//	std::cout << "정렬 된 이후 : " << std::endl;
+//	for (int i = 0; i < 6; i++) {
+//		std::cout << list[i];
 //	}
-//
 //	std::cout << std::endl;
-//
-//	std::cout << "재배치될 멘티는 다음과 같습니다." << std::endl;
-//	for (int i = 0; i < n; i++) {
-//		std::cout << "이름 : " << freshMan[i] << std::endl;
-//	}
-//
-//	std::cout << std::endl;
-//	Relocation(studentCouncil, studentCouncil_Num, freshMan);	// 재배치
 //
 //	return 0;
 //}
